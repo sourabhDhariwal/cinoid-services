cb49599c36db1efb59d34188dd394572589ffa67
sourabhDhariwal <sourabh.dhariwal9094@gmail.com>
2020-01-21 00:28:16 +0530

    add admin panel services

--- app.js
+++ app.js
@@ -4,22 +4,29 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
-
 var app = express();
 app.use(cors())
 
+
+let uploadFile = require('./routes/admin/fileUpload');
+app.use('/upload',uploadFile);
+
 app.io = require('socket.io')();
 var socket =app.io;
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
 	// catch 404 and forward to error handler
 	app.use(function(req, res, next) {
 	  next(createError(404));
 	});
+
+	
 
   socket.on( "connection", function( socket ){
 	    console.log( "A user connected" );
@@ -34,11 +41,11 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.json({
-message: err.message,
-error: err
-});
+ 	 res.status(err.status || 500);
+	  res.json({
+	message: err.message,
+	error: err
+	});
 });
 
 module.exports = app;
